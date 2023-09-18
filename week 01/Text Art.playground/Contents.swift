e9a126f8f4d30ef1a0ec01bea3c1d4439e0d15d2
emikee <eks2063@nyu.edu>
2023-09-18 00:20:14 -0400

    text art

---
+++ week 01/Text Art.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+for i in 1...9 {
+    var dot = String(repeating: "*", count: i)
+    var plus = String(repeating: "+", count: i)
+    var percent = String(repeating: "%", count: i)
+    print("\(dot)   \(plus)   \(percent)   ")
+}
+//going in reverse
+for i in stride(from: 8, to: 0, by: -1) {
+    var dot = String(repeating: "*", count: i)
+    var plus = String(repeating: "+", count: i)
+    var percent = String(repeating: "%", count: i)
+    print("\(dot)   \(plus)   \(percent)   ")
+}
+
+
+//errors: tried using the stride function to let the for loop run in reverse but there is no output at all. I used: for i in stride(from: 1, to: 8, by: -1); the problem was that I had to change the range from 8 to 1 rather than having it the other way around.
