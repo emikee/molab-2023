4133f8b15bfc57fd0b3bd50ea64d6f845106697c
emikee <eks2063@nyu.edu>
2023-10-05 11:53:05 -0400

    music player

--- week 04/Audio App/Audio App/ContentView.swift
+++ week 04/Audio App/Audio App/ContentView.swift
@@ -11,45 +11,51 @@ import SwiftUI
 struct ContentView: View {
     @State private var navPath = NavigationPath()
     let selections: [String: Any] = ["My Favorite Song": PlayFavSong(), "Study Break": PlayCoffeeShop()]
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+    }
     var body: some View {
-        ZStack {
-            Color("DarkBG")
-                .ignoresSafeArea()
-                .overlay(
-                    NavigationStack(path: $navPath) {
-                        VStack(alignment: .center) {
-                            NavigationLink (destination: PlayFavSong()) {
-                                Text("My Favorite Song")
-                            }
-                            .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.631, saturation: 0.678, brightness: 0.599)/*@END_MENU_TOKEN@*/)
-                            .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
-                            .frame(maxWidth: .infinity)
-                            NavigationLink (destination: PlayCoffeeShop()) {
-                                Text("Study Break")
-                            }
-                            .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.631, saturation: 0.678, brightness: 0.599)/*@END_MENU_TOKEN@*/)
-                            .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
-                            .frame(maxWidth: .infinity)
-                            
+        VStack {
+            NavigationStack(path: $navPath) {
+                ZStack {
+                    Color("DarkBG")
+                    Color("DarkBG").ignoresSafeArea()
+                    
+                    VStack(alignment: .center) {
+                        NavigationLink (destination: PlayFavSong()) {
+                            Text("My Favorite Song")
                         }
-                        .navigationTitle("Emily's Space")
-                        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
-                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.631, saturation: 0.138, brightness: 0.927)/*@END_MENU_TOKEN@*/)
-                        .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.391, saturation: 0.822, brightness: 0.705)/*@END_MENU_TOKEN@*/)
+                        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity)
+                        NavigationLink (destination: PlayCoffeeShop()) {
+                            Text("Study Break")
                         }
-                )
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.391, saturation: 0.822, brightness: 0.705)/*@END_MENU_TOKEN@*/)
+                        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity)
+                        
+                    }
+                    .navigationTitle("Emily's Space")
+                    .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                    
+                }
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.631, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/)
+            }
         }
-    
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+struct NavPage_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
--- week 04/Audio App/Audio App/FavSong.swift
+++ week 04/Audio App/Audio App/FavSong.swift
@@ -7,12 +7,17 @@
 import SwiftUI
 import AVFoundation
 
-let favSong = [
-    "92914 - Okinawa (Audio).mp3",
-    "Redbone - Childish Gambino (Guitar Cover) (1).mp3",
-    "life hack Vaundy (Guitar cover).mp3"
-];
+struct Song {
+    let title: String
+    let artist: String
+    let file: String
+    let cover: String
+}
+let song1 = Song(title: "Okinawa", artist: "92914", file: "92914 - Okinawa (Audio).mp3", cover: "Okinawa")
+let song2 = Song(title: "Redbone", artist: "Childish Gambino (Gyoshi Cover)", file: "Redbone - Childish Gambino (Guitar Cover) (1).mp3", cover: "Redbone")
+let song3 = Song(title: "life hack", artist: "Vaundy (Gyoshi Cover)", file: "life hack Vaundy (Guitar cover).mp3", cover: "LifeHack")
 
+let songInfo = [song1, song2, song3]
 // Create a Audio Player given a file stored in the app bundle
 // Detailed documntation on AVAudioPlayer
 //  https://developer.apple.com/documentation/avfaudio/avaudioplayer
@@ -30,53 +35,100 @@ func loadFavSong(_ fileName:String) -> AVAudioPlayer? {
 
 struct PlayFavSong: View {
     @State private var soundIndex = 0
-    @State private var soundFile = favSong[0]
+    @State private var soundFile = songInfo[0].file
+    @State private var soundTitle = songInfo[0].title
+    @State private var soundArtist = songInfo[0].artist
+    @State private var soundCover = songInfo[0].cover
     @State private var player: AVAudioPlayer? = nil
+    @State private var isRotating = 0.0
     var body: some View {
-        TimelineView(.animation) { context in
-            VStack {
-                Text("soundIndex \(soundIndex)")
-                Text("soundFile \(soundFile)")
-                if let player = player {
-                    Text("duration " + String(format: "%.1f", player.duration))
-                    Text("currentTime " + String(format: "%.1f", player.currentTime))
+        ZStack {
+            Color("DarkBG")
+            Color("DarkBG").ignoresSafeArea()
+            
+            TimelineView(.animation) { context in
+                VStack {
+                    Text("MY COLLECTION")
+                        .font(.title)
+                        .padding(.all, 12.0)
+                        .kerning(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                        .padding(/*@START_MENU_TOKEN@*/.bottom, 20.0/*@END_MENU_TOKEN@*/)
+                        
+                    ZStack {
+                        Image("\(soundCover)")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 250, height: 250)
+                            .clipShape(Circle())
+                            .rotationEffect(.degrees(isRotating))
+                            .onAppear{
+                                withAnimation(.linear(duration: 1)
+                                            .speed(0.1).repeatForever(autoreverses: false)) {
+                                        isRotating = 360.0
+                                    }
+                            }
+                        Circle()
+                            .fill(Color(hue: 0.65, saturation: 0.151, brightness: 0.37))
+                            .frame(width: 70, height: 70)
+                        Circle()
+                            .fill(Color.black)
+                            .frame(width: 50, height: 50)
+                    }
+                    .padding(.bottom, 30.0)
+                    
+                    Text("SONG \(soundIndex + 1)")
+                        .kerning(6.0)
+                    Text("\(soundTitle)")
+                        .font(.title)
+                    Text("\(soundArtist)")
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.037, brightness: 0.734))
+                        .padding(.bottom, 20.0)
+//                    if let player = player {
+//                        .brightness(20)
+//                    }
+                    
+                    HStack {
+                        Button {
+                            print("Button Stop")
+                            player?.stop()
+                        } label: {
+                            Image("Stop")
+                                .resizable()
+                                .frame(width: 48.0, height: 48.0)
+                        }
+                        Button{
+                            print("Button Play")
+                            player = loadFavSong(soundFile)
+                            // Loop indefinitely
+                            player?.numberOfLoops = -1
+                            player?.play()
+                        } label: {
+                            Image("Play")
+                                .resizable()
+                                .frame(width: 48.0, height: 48.0)
+                        }
+                        Button{
+                            soundIndex = (soundIndex+1) % songInfo.count
+                            soundFile = songInfo[soundIndex].file;
+                            soundTitle = songInfo[soundIndex].title;
+                            soundArtist = songInfo[soundIndex].artist;
+                            soundCover = songInfo[soundIndex].cover;
+                        } label: {
+                            Image("Skip")
+                                .resizable()
+                                .frame(width: 48.0, height: 48.0)
+                        }
+                    }
+                    
                 }
-                HStack {
-                    Button {
-                        print("Button Stop")
-                        player?.stop()
-                    } label: {
-                        Image("Stop")
-                            .resizable()
-                            .frame(width: 48.0, height: 48.0)
-                    }
-                    Button{
-                        print("Button Play")
-                        player = loadFavSong(soundFile)
-                        print("player", player as Any)
-                        // Loop indefinitely
-                        player?.numberOfLoops = -1
-                        player?.play()
-                    } label: {
-                        Image("Play")
-                            .resizable()
-                            .frame(width: 48.0, height: 48.0)
-                    }
-                    Button{
-                        soundIndex = (soundIndex+1) % favSong.count
-                        soundFile = favSong[soundIndex];
-                    } label: {
-                        Image("Skip")
-                            .resizable()
-                            .frame(width: 48.0, height: 48.0)
-                    }
-                }
+                .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
             }
         }
     }
 }
 
-struct Page1_Previews: PreviewProvider {
+struct MySongs_Previews: PreviewProvider {
     static var previews: some View {
         PlayFavSong()
     }
