0899f3a4fb5b43a863d00aeeab9675bb476587fd
emikee <eks2063@nyu.edu>
2023-10-12 09:13:38 -0400

    week 5dupe

---
+++ week 05/Audio App/Audio App/CoffeeShop.swift
@@ -0,0 +1,82 @@
+import SwiftUI
+import AVFoundation
+
+struct AudioComponent {
+    let audioName: String
+    let file: String
+}
+let bgMusic = AudioComponent(audioName: "Background music", file: "That Kyoto Vibe.mp3")
+let rainSound = AudioComponent(audioName: "Rain", file: "1 MINUTE Relaxing rain sound rain.mp3")
+
+func loadBundleAudio(_ fileName:String) -> AVAudioPlayer? {
+    let path = Bundle.main.path(forResource: fileName, ofType:nil)!
+    let url = URL(fileURLWithPath: path)
+    do {
+        return try AVAudioPlayer(contentsOf: url)
+    } catch {
+        print("loadBundleAudio error", error)
+    }
+    return nil
+}
+
+struct PlayCoffeeShop: View {
+    @State private var soundIndex = 0
+    //    @State private var soundFile = bundleAudio[0]
+    @State private var player: AVAudioPlayer? = nil
+    var body: some View {
+        TimelineView(.animation) { context in
+            VStack {
+                HStack {
+                    Button {
+                        print("Button Stop")
+                        player?.stop()
+                    } label: {
+                        Image("Stop")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                    }
+                    Button{
+                        print("Button Play")
+                        player = loadBundleAudio(bgMusic.file)
+                        // Loop indefinitely
+                        player?.numberOfLoops = -1
+                        player?.play()
+                    } label: {
+                        Image("Play")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                    }
+                    Text("\(bgMusic.audioName)")
+                }
+                HStack {
+                    Button {
+                        print("Button Stop")
+                        player?.stop()
+                    } label: {
+                        Image("Stop")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                    }
+                    Button{
+                        print("Button Play")
+                        player = loadBundleAudio(rainSound.file)
+                        // Loop indefinitely
+                        player?.numberOfLoops = -1
+                        player?.play()
+                    } label: {
+                        Image("Play")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                    }
+                    Text("\(rainSound.audioName)")
+                }
+            }
+        }
+    }
+    
+    struct Page2_Previews: PreviewProvider {
+        static var previews: some View {
+            PlayCoffeeShop()
+        }
+    }
+}
---
+++ week 05/Audio App/Audio App/ContentView.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var navPath = NavigationPath()
+    let selections: [String: Any] = ["My Favorite Song": PlayFavSong(), "Study Break": PlayCoffeeShop()]
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+    }
+    var body: some View {
+        VStack {
+            NavigationStack(path: $navPath) {
+                ZStack {
+                    Color("DarkBG")
+                    Color("DarkBG").ignoresSafeArea()
+                    
+                    VStack(alignment: .center) {
+                        NavigationLink (destination: PlayFavSong()) {
+                            Text("My Favorite Song")
+                                .font(Font.custom("Archivo-Regular.ttf", size: 24))
+                                //.font(.title)
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.391, saturation: 0.822, brightness: 0.705)/*@END_MENU_TOKEN@*/)
+                        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity)
+                        NavigationLink (destination: PlayCoffeeShop()) {
+                            Text("Study Break")
+                                .font(Font.custom("Archivo-Default", size: 24))
+                                //.font(.title)
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.391, saturation: 0.822, brightness: 0.705)/*@END_MENU_TOKEN@*/)
+                        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity)
+                        
+                    }
+                    .navigationTitle("Emily's Space")
+                    .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+                    
+                    
+                }
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.631, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+
+struct NavPage_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
